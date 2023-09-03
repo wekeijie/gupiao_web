d5c10909730ddd111c47876f9850729320724dee
change <2416638110@qq.com>
2023-09-03 10:48:29 +0800

    add card

---
+++ src/views/myHomePage/CardBind.vue
@@ -0,0 +1,79 @@
+<template>
+    <div>
+        <page-header>
+            <template v-slot:headerCenter>银行卡绑定</template>
+        </page-header>
+
+        <div class="line-box flexStart" style="padding:0 15px">
+            <h2>用户姓名</h2>
+            <span>you name</span>
+        </div>
+
+        <div class="line-box flexStart">
+            <h2>身份证号</h2>
+            <span>you name</span>
+
+        </div>
+
+        <v-text-field variant="underlined" v-model="loginData.phone" required clearable label="请输入银行卡号" hide-details="auto"
+            :rules="emailRoules">
+
+            <template v-slot:prepend>
+                <p>银行卡号</p>
+            </template>
+        </v-text-field>
+
+
+        <v-checkbox hide-details="auto" height="auto" color="red" label="设置为默认"></v-checkbox>
+
+        <div class="login-box">
+            <v-btn color="#dd5054" class="mt-4" size="x-large" block @click="handleLogin">
+                添加
+            </v-btn>
+        </div>
+
+    </div>
+</template>
+  
+<script setup>
+import { defineProps, defineEmits, defineExpose, reactive, ref, onMounted, onBeforeUnmount, computed, watch, nextTick } from "vue"
+
+import PageHeader from '../../components/topWrap.vue'
+import { useRouter, useRoute } from "vue-router"
+const $router = useRouter()
+const $route = useRoute()
+const ex11 = ref(true)
+
+const goRouter = (path) => {
+    $router.push(path)
+}
+
+
+</script>
+<style lang="scss" scoped>
+.line-box {
+    padding: 0 15px;
+    border-bottom: solid 0.5px rgb(245, 245, 245);
+    ;
+    line-height: 60px;
+
+    h2 {
+        font-size: 15px;
+        letter-spacing: 0.5px;
+        font-weight: 400px;
+        color: #303133;
+        width: 25%;
+        // color: #2c2e3a;
+    }
+
+    span {
+        color: #7f829a;
+    }
+
+    img {
+        width: 7px;
+        margin-left: 10px;
+    }
+}
+</style>
+  
--- src/views/myHomePage/RealCard.vue
+++ src/views/myHomePage/RealCard.vue
@@ -5,12 +5,34 @@
         </page-header>
         <div class="login-box">
 
-            <div>12312</div>
+            <div v-for="item, index in 3" :key="index" class="card-box">
+                <div class="flexBetween cars-up">
+                    <div class="flexStart card-infor">
+
+                        <img class="list-pay-log" src="../../assets/img/payLog.png" alt="">
+                        <div>
+                            <h3>中国工商很行</h3>
+                            <p>1231231333</p>
+                        </div>
+                    </div>
+                    <h5>you name</h5>
+                </div>
+                <div class="flexBetween card-controls">
+                    <v-checkbox hide-details="auto" height="auto" color="red" label="默认银行卡"></v-checkbox>
+                    <div class="flexEnd card-cont-item">
+                        <p><v-icon hide-details="auto" icon="mdi-delete" size="x-small"></v-icon><span>解绑</span></p>
+                        <p><v-icon hide-details="auto" height="auto" icon="mdi-eye" size="x-small"></v-icon><span>详情</span>
+                        </p>
+
+                    </div>
+                </div>
+
+            </div>
 
         </div>
 
         <div class="login-box">
-            <v-btn color="#fb5c39" class="mt-4" size="x-large" block @click="handleLogin" prepend-icon="+">
+            <v-btn color="#dd5054" class="mt-4" size="x-large" block @click="handleLogin" prepend-icon="mdi-plus">
                 添加银行卡
             </v-btn>
         </div>
@@ -65,6 +87,75 @@ const goSign = () => {
 };
 </script>
 <style lang="scss" scoped>
+.card-cont-item {
+    margin-left: 10px;
+
+    p {
+        margin-left: 10px;
+        font-size: 13px;
+    }
+
+    span {
+
+        margin-left: 3px;
+        font-size: 13px;
+    }
+}
+
+.card-controls {
+    padding: 1px 0;
+    border-top: solid 1px #e1e1e1;
+
+
+}
+
+::v-deep .v-checkbox .v-selection-control {
+    min-height: 10px !important;
+    color: #606266;
+
+    .v-selection-control--density-default {
+        --v-selection-control-size: none,
+    }
+
+    .v-label {
+        font-size: 12px !important;
+    }
+}
+
+.card-box {
+    border: solid 1px #e4d6d0;
+    border-radius: 10px;
+    padding: 10px;
+    box-shadow: 0 0 10px 0 rgba(0, 0, 0, .1);
+    margin-top: 10px;
+}
+
+.card-infor {
+    img {
+        width: 45px;
+        margin-right: 10px;
+    }
+
+    h3 {
+        font-size: 16px;
+        font-weight: bolder;
+    }
+
+    p {
+        font-size: 14px;
+        margin-top: 5px;
+    }
+
+    h5 {
+        font-size: 15px;
+        font-weight: bolder;
+    }
+}
+
+.cars-up {
+    padding: 10px 0;
+}
+
 .login-box {
     padding: 20px 20px 0;
     text-align: center;
