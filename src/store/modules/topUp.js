f06ffedb153ad7d790ea74137f386c1f9c60853a
Your Name <you@example.com>
2023-12-02 09:06:40 +0000

    6

--- src/store/modules/topUp.js
+++ src/store/modules/topUp.js
@@ -1,37 +1,39 @@
-import { listApi,putApi,getRechargeApi } from "@/api/topUpApi"
+import { listApi, putApi, getRechargeApi } from "@/api/topUpApi";
 export default {
   namespaced: true,
   state: () => ({
-    list:{},
-    data:{},
-    recharge:[]
+    list: {},
+    data: {},
+    recharge: [],
   }),
   mutations: {
-    setList(state,rsp){
-        state.list = rsp
+    setList(state, rsp) {
+      state.list = rsp;
     },
-    setData(state,ifno){
-        state.data = info
+    setData(state, ifno) {
+      state.data = info;
     },
-    setRecharge(state,rsp){
-        state.recharge = rsp
-    }
+    setRecharge(state, rsp) {
+      state.recharge = rsp;
+    },
   },
   actions: {
-    async getList(content){
-        const rsp = await listApi()
-        content.commit('setList',rsp)
+    async getList(content) {
+      const rsp = await listApi();
+      content.commit("setList", rsp);
     },
-    updateData(content,info){
-        content.commit('setData',info)
+    updateData(content, info) {
+      content.commit("setData", info);
     },
-    subPut(content,data){
-        return putApi(data)
+    subPut(content, data) {
+      return putApi(data);
     },
-    async getRecharge(content,info){
-        const rsp = await getRechargeApi(info.page,info.status)
-        content.commit('setRecharge',rsp)
-        return rsp
-    }
-  }
-}
+    async getRecharge(content, info) {
+      const rsp = await getRechargeApi(info.page, info.status);
+      if (rsp.length > 0) {
+        content.commit("setRecharge", rsp);
+      }
+      return rsp;
+    },
+  },
+};
