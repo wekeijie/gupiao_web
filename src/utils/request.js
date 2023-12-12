4ae5ca55f3515a45e796b5db50dc1e1f54bf36fe
Your Name <you@example.com>
2023-12-12 11:21:13 +0000

    change timeout

--- src/utils/request.js
+++ src/utils/request.js
@@ -4,7 +4,7 @@ import { store } from "@/store";
 
 const service = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  timeout: 5000,
+  timeout: 10000,
 });
 
 service.interceptors.request.use(
