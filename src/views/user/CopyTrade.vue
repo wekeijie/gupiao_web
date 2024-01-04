74421c0ee1d2e1ec497dfd5e7ac1c87853f20373
Your Name <you@example.com>
2024-01-04 05:06:50 +0000

    change avatar

--- src/views/user/CopyTrade.vue
+++ src/views/user/CopyTrade.vue
@@ -31,7 +31,7 @@
       <div class="bg-white rounded-lg py-3 px-2 relative">
         <img
           v-if="store.state.trade.info.avatar"
-          src="https://cdn.cicctx.com/20231203/XpTV53aqVg1Sz0884jmHiY34WIP9pVxEQnKsSrY2.png"
+          :src="store.state.trade.info.avatar"
           width="90"
           height="90"
           class="rounded-circle absolute avatar-top-left"
