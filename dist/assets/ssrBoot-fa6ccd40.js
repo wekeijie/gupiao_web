48b0f32cb65058b7d4b9c4f93dbdac5312b7349a
Your Name <you@example.com>
2024-10-08 13:43:55 +0000

    update logo name

---
+++ dist/assets/ssrBoot-fa6ccd40.js
@@ -0,0 +1,1 @@
+import{ab as t,O as e,q as s,bO as n}from"./index-c37ba9da.js";function i(){const o=t(!1);return e(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:s(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
